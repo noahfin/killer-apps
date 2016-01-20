56950de5c74cc95f30818076ae763adf93a34ff0
Noah Finnerman <nonothetoad@gmail.com>
2016-01-20 14:51:19 -0500

    Add login, signup and comments HTML

--- server.rb
+++ server.rb
@@ -2,10 +2,18 @@ require "bcrypt"
 
 module Forum
 	class Server < Sinatra::Base
+		set :method_orverride, true
 
 		get '/' do
 			erb :index
 		end
+		get '/signup' do
+			erb :signup
+		end
+		get '/login' do
+			erb :login
+		end
+
 
 		get '/fourm' do
 		erb :fourm
