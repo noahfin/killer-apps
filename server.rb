be7271fdaf882081990330db834e7056039742b1
Noah Finnerman <nonothetoad@gmail.com>
2016-01-24 15:48:09 -0500

    Fixed error for nil class in comments

--- server.rb
+++ server.rb
@@ -22,7 +22,7 @@ module Forum
 			markdown.render(content)
 		end
 
-    	
+    	@@conn ||= PG.connect(dbname: "killer-apps")
 
 		get '/' do
 			erb :index
@@ -45,8 +45,8 @@ module Forum
     	terms = params["terms"]
 
     	password = BCrypt::Password.create(params["password"])
-    	conn = PG.connect(dbname: "killer-apps")
-      new_user =	conn.exec_params(
+    	
+      new_user =	@@conn.exec_params(
     		"INSERT INTO users (fname, lname, email, city, state, terms, password) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id;",[fname, lname, email, city, state, terms, password] )
     	@contact_submitted = true
       session["user_id"] = new_user.first["id"].to_i    
@@ -56,8 +56,7 @@ module Forum
     post '/login' do
 			password = params["password"]
       email = params["email"]
-      conn = PG.connect(dbname: "killer-apps")
-      user = conn.exec_params("SELECT * FROM users WHERE email = $1",[email]).first
+      user = @@conn.exec_params("SELECT * FROM users WHERE email = $1",[email]).first
         
       user_password = BCrypt::Password.new(user["password"])
      
@@ -86,16 +85,15 @@ module Forum
 			title = params["title"]
 			post_id = params["post"]
       message = params["message"]
-      conn = PG.connect(dbname: "killer-apps")
       if current_user == nil || current_user['id'].to_i < 1
       	'<a href="/login">You need to signup or login to post</a>'
       else
-      post_id = conn.exec_params(
+      post_id = @@conn.exec_params(
     		"INSERT INTO post (post_title, post_content, post_by, post_topic) VALUES ($1, $2, $3, $4  )RETURNING id;",[title, message, current_user['id'], topic])
          post_id =  post_id.first["id"].to_i  
         @post = conn.exec_params("SELECT * FROM post WHERE id = $1", [post_id]).to_a
      
-      conn.exec_params(
+      @@conn.exec_params(
     		"INSERT INTO topics (topic_subject, topic_by  ) VALUES ($1, $2  );",[topic, current_user['id']]  )
 
       
@@ -105,9 +103,8 @@ module Forum
 
 		get '/post/:id' do 
 			 post_id = params["id"].to_i
-			conn = PG.connect(dbname: "killer-apps")
-			@post = conn.exec_params("SELECT * FROM post WHERE id = #{post_id};").to_a
-      @comments =conn.exec_params("SELECT * FROM comments WHERE comment_in = #{post_id}").to_a
+			@post = @@conn.exec_params("SELECT * FROM post WHERE id = #{post_id};").to_a
+      @comments = @@conn.exec_params("SELECT * FROM comments WHERE comment_in = $1",[post_id]).to_a
       	
       	@id_array = []
       	@comments.each do |comment|
@@ -115,7 +112,7 @@ module Forum
 			end
 			@comment_names= []
 			@id_array.each_with_index do |id, i|
-			@comment_names.push(conn.exec_params("SELECT (fname, lname) FROM users WHERE id = $1",[@id_array[i]]).first)
+			@comment_names.push(@@conn.exec_params("SELECT (fname, lname) FROM users WHERE id = $1",[@id_array[i]]).first)
 			# @id_array.each do |id|
 			  
 			end
@@ -126,7 +123,7 @@ module Forum
 			end
 			@name = []
 			@id_array.each do |id|
-			 @name.push(conn.exec_params("SELECT (fname, lname) FROM users WHERE id = $1",[id]).first)
+			 @name.push(@@conn.exec_params("SELECT (fname, lname) FROM users WHERE id = $1",[id]).first)
 			end
         	
 			#@comments = conn.exec_params("SELECT * FROM comments JOIN post ON comment.comment_in = post.id WHERE comment_in = #{post_id}" ).to_a
@@ -137,31 +134,32 @@ module Forum
 
 		get '/user/:id' do
 			post_id = params["id"]
-			conn = PG.connect(dbname: "killer-apps")
-			@post = conn.exec_params("SELECT * FROM post JOIN users ON post.post_by = users.id WHERE post_by = #{post_id}" ).to_a
+			@post = @@conn.exec_params("SELECT * FROM post JOIN users ON post.post_by = users.id WHERE post_by = $1",[	post_id ] ).to_a
 			erb :fourm
 		end   #= $1",[session["user_id"]] 
 
 		post '/comment' do 
+			if current_user == nil || current_user['id'].to_i < 1
+      	'<a href="/login">You need to signup or login to commentt</a>'
+      else
 			  comment = params["message"]
 			  post_id = params["post_id"].to_i
-			   conn = PG.connect(dbname: "killer-apps")
 			 #  user = conn.exec_params("SELECT * FROM users WHERE email = $1",[email]).first
-      	conn.exec_params(
+      	@@conn.exec_params(
     		"INSERT INTO comments (comment_content , comment_by, comment_in) VALUES ($1, $2, $3 );",[comment,  current_user['id'], post_id])
-      	"Your comment was posted sucessfuly"
-		end
+      	'Your comment was posted sucessfuly' +  '<a href="/post/' + post_id.to_s + '">View</a>' 
+		  end
+	  end
 
 		get '/fourm' do
-			conn = PG.connect(dbname: "killer-apps")
-			@post = conn.exec_params("select * from post;").to_a
+			@post = @@conn.exec_params("select * from post;").to_a
 			@id_array = []
 			@post.each do |post|
 				@id_array.push( post['post_by'].to_i) 
 			end
 			@name = []
 			@id_array.each do |id|
-			 @name.push(conn.exec_params("SELECT (fname, lname) FROM users WHERE id = $1",[id]).first)
+			 @name.push(@@conn.exec_params("SELECT (fname, lname) FROM users WHERE id = $1",[id]).first)
 			end
 
 		erb :fourm
@@ -169,33 +167,29 @@ module Forum
 
 	  get '/topic/:topic' do
 	  	topic = params['topic']
-	  	conn = PG.connect(dbname: "killer-apps")
-			@post =  conn.exec_params("SELECT * FROM post WHERE post_topic LIKE $1",[topic] )
+			@post =  @@conn.exec_params("SELECT * FROM post WHERE post_topic LIKE $1",[topic] )
 			erb :fourm
 
 	  end
 
 	  get '/edit/:id' do
 	  	post_id = params["id"].to_i
-			conn = PG.connect(dbname: "killer-apps")
-			@post = conn.exec_params("SELECT * FROM post WHERE id = $1;",[post_id]).to_a
+			@post = @@conn.exec_params("SELECT * FROM post WHERE id = $1;",[post_id]).to_a
 	  	erb :edit
 	  end
 
 
 	  get '/delete/:id' do
 	  	 post_id = params["id"].to_i
-			conn = PG.connect(dbname: "killer-apps")
-			@post = conn.exec_params("SELECT * FROM post WHERE id = #{post_id};").to_a
-		 @comments =conn.exec_params("SELECT * FROM comments WHERE comment_in = #{post_id}").to_a		 	
+			@post =  @@conn.exec_params("SELECT * FROM post WHERE id = $1;",[post_id]).to_a
+		 @comments =@@conn.exec_params("SELECT * FROM comments WHERE comment_in = $1",[post_id]).to_a		 	
 	  	erb :delete
 	  end
 	  post '/delete/:id' do
 	  	post_id = params['id'].to_i
-	  	conn = PG.connect(dbname: "killer-apps")
-	    @post = conn.exec_params("SELECT * FROM post WHERE id = #{post_id};").to_a
+	    @post = @@conn.exec_params("SELECT * FROM post WHERE id = #{post_id};").to_a
 			   if @post[0]['post_by'] == current_user['id']
-			 conn.exec_params("DELETE FROM POST WHERE id = $1 ",[post_id])
+			 @@conn.exec_params("DELETE FROM POST WHERE id = $1 ",[post_id])
 			 'you were able to delete'
 			else 
 				'You were not allowed to delete'
@@ -211,27 +205,27 @@ module Forum
 	  	topic = params["topic"]
 			title = params["title"]
       message = params["message"]
-      conn = PG.connect(dbname: "killer-apps")
+
       if  topic.length < 1 && title.length <1 && message.length <1
       	"Data error"
         elsif topic.length < 1 && title.length <1 && message.length >= 1
         'message updated'   	
-      	 conn.exec_params("UPDATE post SET post_content =$1 WHERE id = $2;",[ message,post_id ])
+      	 @@conn.exec_params("UPDATE post SET post_content =$1 WHERE id = $2;",[ message,post_id ])
       	elsif  topic.length < 1 && title.length > 0 && message.length > 1
       		'message and title updated'
-      		 conn.exec_params("UPDATE post SET post_content =$1, post_title = $2 WHERE id = $3;",[ message, title, post_id ])
+      		 @@conn.exec_params("UPDATE post SET post_content =$1, post_title = $2 WHERE id = $3;",[ message, title, post_id ])
       	elsif  topic.length > 1  && title.length  < 1  && message.length > 1	 
       		'topic and message updated'
-      		 conn.exec_params("UPDATE post SET post_content =$1, post_topic = $2 WHERE id = $3;",[ message, topic, post_id ])
+      		 @@conn.exec_params("UPDATE post SET post_content =$1, post_topic = $2 WHERE id = $3;",[ message, topic, post_id ])
       	elsif  topic.length > 0  && title.length > 0  && message.length > 1	 
-      		 conn.exec_params("UPDATE post SET post_content =$1, post_topic = $2, post_title = $3  WHERE id = $4;",[ message, topic, title, post_id ])
+      		 @@conn.exec_params("UPDATE post SET post_content =$1, post_topic = $2, post_title = $3  WHERE id = $4;",[ message, topic, title, post_id ])
       		 'topic message and title were updated'
       	
       		 elsif topic.length > 1 && title.length <1 && message.length < 1      		 	
-          conn.exec_params("UPDATE post SET post_topic =$1 WHERE id = $2;",[ topic,post_id ])  	
+          @@conn.exec_params("UPDATE post SET post_topic =$1 WHERE id = $2;",[ topic,post_id ])  	
       elsif topic.length < 1 && title.length >1 && message.length < 1
         'title updated'   	
-      	 conn.exec_params("UPDATE post SET post_title =$1 WHERE id = $2;",[ title,post_id ])
+      	 @@conn.exec_params("UPDATE post SET post_title =$1 WHERE id = $2;",[ title,post_id ])
       else
       	"Error"
 
